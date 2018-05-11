33a86ab15a53e82b0343a6e7f4c0e9d44a6d2f4c
AGFlorence <36081478+AGFlorence@users.noreply.github.com>
2018-05-11 16:06:16 +0100

    anime -website
    version 21 - fixing and closing modals

--- functions.js
+++ functions.js
@@ -1,10 +1,20 @@
-function Toogle(){
+function OpenTC(){
   console.log("Expanding information")
-  let modal = document.getElementById('myModal');
+  let modal = document.getElementById('TC');
   modal.style.display = "block";
 }
-function close(){
+function CloseTC(){
   console.log("hiding information")
-  let modal = document.getElementById('myModal');
+  let modal = document.getElementById('TC');
+  modal.style.display = "none";
+}
+function OpenLang(){
+  console.log("Expanding information")
+  let modal = document.getElementById('Lang');
+  modal.style.display = "block";
+}
+function CloseLang(){
+  console.log("hiding information")
+  let modal = document.getElementById('Lang');
   modal.style.display = "none";
 }
